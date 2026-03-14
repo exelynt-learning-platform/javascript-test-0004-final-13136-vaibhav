db9fcf9d1f73b8ffcde4a694c5fb91accbcf7f34
Vaibhav Dhotre <99244402+vaibhav3022@users.noreply.github.com>
2026-03-14 23:01:30 +0530

    Refactor row output to remove trailing spaces
    Trimmed trailing spaces from each row output.

--- Pattern4.js
+++ Pattern4.js
@@ -1,11 +1,9 @@
-
 let n = 4;
 
 for (let i = 1; i <= 7; i++) {
     let row = "";
 
     for (let j = 1; j <= 7; j++) {
-
         let top = i - 1;
         let left = j - 1;
         let bottom = 7 - i;
@@ -13,8 +11,8 @@ for (let i = 1; i <= 7; i++) {
 
         let min = Math.min(top, left, bottom, right);
 
-        row += (n - min);
+        row += (n - min) + " ";
     }
 
-    console.log(row);
+    console.log(row.trim());
 }
