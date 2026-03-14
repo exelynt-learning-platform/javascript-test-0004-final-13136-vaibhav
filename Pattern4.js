4aafcc1c172dd4b0d2f5a22f42c1561826c20ddc
Vaibhav Dhotre <99244402+vaibhav3022@users.noreply.github.com>
2026-03-14 14:06:05 +0530

    Create Pattern4.js

---
+++ Pattern4.js
@@ -0,0 +1,20 @@
+
+let n = 4;
+
+for (let i = 1; i <= 7; i++) {
+    let row = "";
+
+    for (let j = 1; j <= 7; j++) {
+
+        let top = i - 1;
+        let left = j - 1;
+        let bottom = 7 - i;
+        let right = 7 - j;
+
+        let min = Math.min(top, left, bottom, right);
+
+        row += (n - min);
+    }
+
+    console.log(row);
+}
